309f8f05f00629cd74bb75c2c95b4e8bf7bc3afa
原田勇大 <haradayuta@haradayuudainoMacBook-Pro.local>
2018-11-20 14:18:25 +0900

    second commit

--- bot.py
+++ bot.py
@@ -15,3 +15,20 @@ line_bot_api = LineBotApi('tyLwEVCKmRitC5qekJUKcbVIv/evvoVUiaCULg/Mf3IQxiTRPasGW
 handler = WebhookHandler('78a3a615d02a551e4982e0e6ce63b1d4')
 
 @app.route("/callback",methods=['POST'])
+def callback():
+    signature=request.headers['X-Line-Signature']
+    body=request.get_data(as_text=True)
+    app.logger.info("Request body: "+body)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
+if __name__=="__main__":
+    app.run()
